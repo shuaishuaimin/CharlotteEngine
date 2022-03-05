588fbd4677951522f063b5e90d6e8d0637238d07
陈有民 <862879864@qq.com>
2022-03-05 17:41:51 +0800

    commit

---
+++ Shaders/Offset.hlsl
@@ -0,0 +1,46 @@
+//***************************************************************************************
+// color.hlsl by Frank Luna (C) 2015 All Rights Reserved.
+//
+// Transforms and colors geometry.
+//***************************************************************************************
+
+cbuffer cbPerObject : register(b0)
+{
+	float4x4 gWorldViewProj; 
+	float gOffest;
+};
+
+struct VertexIn
+{
+	float3 PosL  : POSITION;
+    float4 Color : COLOR;
+	float4 Normal : NORMAL;
+};
+
+struct VertexOut
+{
+	float4 PosH  : SV_POSITION;
+    float4 Color : COLOR;
+	float4 Normal : NORMAL;
+};
+
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout;
+	
+	vin.PosL = mul(vin.PosL, sin(gOffest));
+	// Transform to homogeneous clip space.
+	vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
+	
+	// Just pass vertex color into the pixel shader.
+    vout.Color = vin.Normal;
+    
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+    return pin.Color;
+}
+
+
