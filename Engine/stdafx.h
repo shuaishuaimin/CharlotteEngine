53746985c9f0202457620fde303bf394dfe263fa
陈有民 <862879864@qq.com>
2022-03-09 18:34:02 +0800

    commit .h

--- Engine/stdafx.h
+++ Engine/stdafx.h
@@ -1,15 +1,8 @@
 #pragma once
 
-#if PLATFORM_WINDOWS
 
-#include <windows.h>
-#include <wrl.h>
-#include <dxgi1_4.h>
-#include <d3d12.h>
-#include <D3Dcompiler.h>
-#include <DirectXPackedVector.h>
-#include <DirectXColors.h>
-#include <DirectXCollision.h>
+
+
 #include <string>
 #include <memory>
 #include <algorithm>
@@ -21,10 +14,33 @@
 #include <fstream>
 #include <sstream>
 #include <cassert>
-#include "d3dx12.h"
-#include "DDSTextureLoader.h"
+
+#include "FRender.h"
 #include "Math.h"
 #include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
+#include "gtc/quaternion.hpp"
+#include "BaseStructAllPlatform.h"
+#include "FMathHelper.h"
+#include "Singleton.h"
+#include "FGameTimer.h"
 
+
+#if PLATFORM_WINDOWS
+#include <windows.h>
+#include <wrl.h>
+#include <dxgi1_4.h>
+#include <d3d12.h>
+#include <D3Dcompiler.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#include "d3dx12.h"
+#include "DDSTextureLoader.h"
+#include "WinBaseData.h"
+#include "FDataProcessor.h"
+#include "FAssetSystem.h"
+#include "FCamera.h"
+#include "FScene.h"
 #endif
 	
