6f766d3fae537757f1cf63235a7ac46153c91bfd
陈有民 <862879864@qq.com>
2022-03-21 14:52:46 +0800

    commit use normal

--- Shaders/defalut.hlsl
+++ Shaders/defalut.hlsl
@@ -56,7 +56,8 @@ VertexOut VS(VertexIn vin)
 float4 PS(VertexOut pin) : SV_Target
 {
 	float4 diffuseAlbedo = gDiffuseMap.Sample(gsamLiner, pin.TexC);
-	
+	float4 normal = gNormalMap.Sample(gsamLiner, pin.TexC);
+	float4 Result = float4( diffuseAlbedo.x*normal.x, diffuseAlbedo.y * normal.y, diffuseAlbedo.z * normal.z, 1.0f);
     return diffuseAlbedo;
 }
 
